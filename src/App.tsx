632454217223af34f3d3b7c6d2aa11543143e7b7
ha4219 <ha4219@naver.com>
2023-02-18 00:12:11 +0900

    FEAT ko -> en

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
+import loadData from './data.json'
 import './App.css';
 
+
+const DEFAULT = 10;
+
 function App() {
+  const allData = loadData.data;
+
+  const [index, setIndex] = React.useState(0);
+  const [page, setPage] = React.useState(0);
+  const [isWrong, setIsWrong] = React.useState(false);
+
+
+  const cur = page * DEFAULT + index;
+
+
+  const handleSubmit = (e: React.SyntheticEvent): void => {
+    e.preventDefault();
+    const target = e.target as typeof e.target & {
+      answer: {value: string}
+    };
+    if (target.answer.value === allData[cur].en) {
+      target.answer.value = "";
+      if (index === 9) {
+        setPage(prev => prev + 1);
+        setIndex(0);
+      } else {
+        setIndex(prev => prev + 1);
+        
+      }
+    } else {
+      setIsWrong(true);
+      const utter = new SpeechSynthesisUtterance(allData[cur].en);
+      window.speechSynthesis.speak(utter);
+    }
+  }
+
+  const handleRefresh = (e: React.MouseEvent<HTMLButtonElement>): void => {
+    e.preventDefault();
+    setIndex(0);
+    setIsWrong(false);
+  }
+
+  React.useEffect(() => {
+    window.speechSynthesis.getVoices();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isWrong ? 
+        <div className='wrong'>
+          <div>틀림</div>
+          <div>
+            <span>answer:</span>
+            <b> {allData[cur].en}</b>
+          </div>
+          <div>
+            <button onClick={handleRefresh} autoFocus>refresh</button>
+          </div>
+        </div> : 
+        <form onSubmit={handleSubmit} className='continue'>
+          <div className='full-div'>
+            {allData[cur].ko}
+          </div>
+          <input type='text' className='input' name='answer' autoComplete='off' autoFocus/>
+          <div className='half-div'>
+            {cur}
+          </div>
+        </form>
+      }
     </div>
   );
 }
